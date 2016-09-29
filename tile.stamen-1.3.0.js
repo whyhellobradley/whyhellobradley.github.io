2f2b81f29500949edca4b3788ee512e9a2af29be
Bradley Dean Jones <iambradleydean@gmail.com>
2016-09-29 18:07:50 -0400

    Update tile.stamen-1.3.0.js

--- tile.stamen-1.3.0.js
+++ tile.stamen-1.3.0.js
@@ -13,10 +13,7 @@ var SUBDOMAINS = "a. b. c. d.".split(" "),
             "minZoom":      minZoom,
             "maxZoom":      maxZoom,
             "attribution":  [
-                'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
-                'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
-                'Data by <a href="http://openstreetmap.org/">OpenStreetMap</a>, ',
-                'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+                'Leaflet map by <a href="http://bradleydeanjones.com/">Bradley Dean Jones</a>'
             ].join("")
         };
     },
