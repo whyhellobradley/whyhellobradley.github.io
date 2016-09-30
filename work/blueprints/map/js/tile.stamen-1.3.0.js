1feb8e2a1f2bfd99f77b021cc62c5f5bf50d7a54
Bradley Dean Jones <iambradleydean@gmail.com>
2016-09-29 21:49:19 -0400

    Update tile.stamen-1.3.0.js

--- work/blueprints/map/js/tile.stamen-1.3.0.js
+++ work/blueprints/map/js/tile.stamen-1.3.0.js
@@ -13,10 +13,7 @@ var SUBDOMAINS = "a. b. c. d.".split(" "),
             "minZoom":      minZoom,
             "maxZoom":      maxZoom,
             "attribution":  [
-                'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
-                'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
-                'Data by <a href="http://openstreetmap.org/">OpenStreetMap</a>, ',
-                'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+                'Map by <a href="http://bradleydeanjones.com/">Bradley Dean Jones</a>'
             ].join("")
         };
     },
@@ -68,10 +65,7 @@ for (var i = 0; i < odbl.length; i++) {
 
     PROVIDERS[key].retina = true;
     PROVIDERS[key].attribution = [
-        'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
-        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
-        'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
-        'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+        'Map by <a href="http://bradleydeanjones.com/">Bradley Dean Jones</a>'
     ].join("");
 }
 
